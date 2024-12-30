42c9880bef814292b62a06c0eccbfc4b614a36c0
Kaline Dutra <kalineduttra@gmail.com>
2024-12-30 11:42:45 -0300

    add custom command legaEntityform

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,4 +13,18 @@ Cypress.Commands.add('individualForm', () => {
     cy.get('.botao-formulario').click()
 })
 
-//           build: npm run build
+Cypress.Commands.add('legalEntityForm', () => {
+    cy.get('#pessoa-juridica').click()
+    cy.get('#name').type(Cypress.env('VALID_NAME'))
+    cy.get('#data').type(Cypress.env('VALID_DATE'))
+    cy.get('.formulario__email > input').type(Cypress.env('VALID_EMAIL'))
+    cy.get('#cnpj').type(Cypress.env('VALID_CNPJ'))
+    cy.get('#telefone').type(Cypress.env('VALID_PHONE_NUMBER'))
+    cy.get('#cep').type(Cypress.env('VALID_CEP'))
+    cy.get('.formulario__endereco_cidade').type(Cypress.env('VALID_CITY'))
+    cy.get('.formulario__endereco_logradouro').type(Cypress.env('VALID_STREET'))
+    cy.get('.formulario__endereco_bairro').type(Cypress.env('VALID_NEIGHBORHOOD'))
+    cy.get('.formulario__endereco_estado').type(Cypress.env('VALID_STATE'))
+    cy.get('.formulario__endereco_numero').type(Cypress.env('VALID_ADDRESS_NUMBER'))
+    cy.get('.botao-formulario').click()
+})
